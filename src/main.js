d11110416ca3bc7b0765cc0dbb213a804630a308
hidesh <hidesh@live.dk>
2023-05-31 09:27:22 +0200

    index.html done and terrazzogulv

--- src/main.js
+++ src/main.js
@@ -1,70 +1,27 @@
-const carouselContainer = document.querySelector('.carousel-container');
-const carouselPrevBtn = document.querySelector('.carousel-prev');
-const carouselNextBtn = document.querySelector('.carousel-next');
-const carouselDots = document.querySelector('.carousel-dots');
-const carouselItems = document.querySelectorAll('.carousel-item');
+// onclick method for navbar
+// Add an onclick event to the dropdown button
+const dropdownButton = document.getElementById('dropdownButton');
+const dropdownMenu = document.getElementById('dropdownMenu');
+const backButtonDDMenu = document.getElementById('backButtonForDDMenu');
+const navbar = document.getElementById('navbar');
 
-let currentIndex = 0;
-let itemsToShow = 1;
-
-function updateCarousel() {
-  const carouselWidth = carouselContainer.clientWidth;
-  const itemWidth = carouselWidth / itemsToShow;
-  carouselContainer.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+function toggledropdownMenu() {
+  dropdownMenu.classList.toggle('hidden');
+  dropdownButton.classList.toggle('rotate-180');
+  backButtonDDMenu.classList.toggle('hidden');
+  dropdownMenu.classList.toggle('animate__slideInRight');
+  dropdownMenu.classList.toggle('animate__animated');
 }
 
-function updateItemsToShow() {
-  if (window.innerWidth >= 1024) {
-    itemsToShow = 4;
-  } else if (window.innerWidth >= 768) {
-    itemsToShow = 3;
-  } else if (window.innerWidth >= 640) {
-    itemsToShow = 2;
-  } else {
-    itemsToShow = 1;
-  }
+function toggleResponsiveMenu() {
+  navbar.classList.toggle('max-sm:hidden');
+  navbar.classList.toggle('absolute');
+  navbar.classList.toggle('inset-x-0');
+  navbar.classList.toggle('z-10');
+  
+
+    navbar.classList.add('animate__slideInRight');
 }
 
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + carouselItems
-.length) % carouselItems.length;
-updateCarousel();
-updateDots();
-}
 
-function nextSlide() {
-currentIndex = (currentIndex + 1) % carouselItems.length;
-updateCarousel();
-updateDots();
-}
 
-function dotClick(index) {
-currentIndex = index;
-updateCarousel();
-updateDots();
-}
-
-function updateDots() {
-const dots = Array.from(carouselDots.children);
-dots.forEach((dot, index) => {
-dot.classList.toggle('active', index === currentIndex);
-});
-}
-
-carouselPrevBtn.addEventListener('click', prevSlide);
-carouselNextBtn.addEventListener('click', nextSlide);
-
-carouselItems.forEach((item, index) => {
-const dot = document.createElement('div');
-dot.classList.add('carousel-dot');
-dot.addEventListener('click', () => dotClick(index));
-carouselDots.appendChild(dot);
-});
-
-window.addEventListener('resize', () => {
-updateItemsToShow();
-updateCarousel();
-});
-
-updateItemsToShow();
-updateCarousel();
