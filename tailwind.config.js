8ff0afd40a25f7a469da26cc12821fc11ed74e24
hidesh <hidesh@live.dk>
2023-06-01 14:33:50 +0200

    included fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-      dtBlue: '#273940',
-      dtOrange: '#E47127',
-      dtBrown: '#BC5637',
-      dtGray: '#607D8B',
-      dtLightGray: '#D9D9D9',
+        dtBlue: '#273940',
+        dtOrange: '#E47127',
+        dtBrown: '#BC5637',
+        dtGray: '#607D8B',
+        dtLightGray: '#D9D9D9',
+      },
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
       },
     },
   },
